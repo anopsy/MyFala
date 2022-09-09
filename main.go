271a18d7c08d29fd8d3905c2e2dccb06181790a3
anopsy <anopsy28@gmail.com>
2022-09-09 18:54:12 +0200

    pupulating db added

--- main.go
+++ main.go
@@ -20,8 +20,60 @@ const (
 	dbname = "surf_spots"
 )
 
-//TODO function that takes data from API and saves it to a data base
-//database of lat/long for dutch coast
+// TODO function that takes data from API and saves it to a data base
+// database of lat/long for dutch coast
+type Location struct {
+	Id   int
+	Name string
+	Lat  string
+	Long string
+}
+
+func getLocation() []Location {
+
+	listLocation := make([]Location, 0)
+	//database query for location
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"dbname=%s sslmode=disable",
+		host, port, user, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected!")
+
+	rows, err := db.Query("SELECT id, name, lat, long FROM surf_spots")
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var spot Location
+		err = rows.Scan(&spot.Id, &spot.Name, &spot.Lat, &spot.Long)
+		if err != nil {
+			panic(err)
+		}
+
+		listLocation = append(listLocation, spot)
+
+	}
+
+	err = rows.Err()
+	if err != nil {
+		panic(err)
+
+	}
+
+	return listLocation
+
+}
 
 type Meta struct {
 	Cost         int
@@ -49,22 +101,22 @@ type WindConditions struct {
 	Meta  Meta
 }
 
-func windAtLocation(x, y float64) WindConditions {
+func windAtLocation(x, y string) WindConditions {
 	start := time.Now()
 	startU := start.Unix()
 	end := start.Add(time.Hour * 24)
 	endU := end.Unix()
 
 	params := url.Values{}
-	params.Add("lat", fmt.Sprintf("%f", 52.4907))
-	params.Add("lng", fmt.Sprintf("%f", 4.6023))
+	params.Add("lat", x)
+	params.Add("lng", y)
 	params.Add("start", fmt.Sprintf("%d", startU))
 	params.Add("end", fmt.Sprintf("%d", endU))
 	params.Add("params", "windSpeed")
 	url := "https://api.stormglass.io/v2/weather/point?"
 
 	meClient := http.Client{
-		Timeout: time.Second * 2, // Timeout after 2 seconds
+		Timeout: time.Second * 10, // Timeout after 2 seconds
 
 	}
 	req, err := http.NewRequest(http.MethodGet, url+params.Encode(), nil)
@@ -72,7 +124,7 @@ func windAtLocation(x, y float64) WindConditions {
 		log.Fatal(err)
 	}
 
-	req.Header.Add("Authorization", "f691cda0-015f-11ed-9a2a-0242ac130002-f691ce54-015f-11ed-9a2a-0242ac130002")
+	req.Header.Add("Authorization", "4c4d2d92-3050-11ed-b970-0242ac130002-4c4d2e00-3050-11ed-b970-0242ac130002")
 
 	res, getErr := meClient.Do(req)
 	if getErr != nil {
@@ -112,22 +164,22 @@ type Waves struct {
 	Meta  Meta
 }
 
-func swellAtLocation(x, y float64) Waves {
+func swellAtLocation(x, y string) Waves {
 	start := time.Now()
 	startU := start.Unix()
 	end := start.Add(time.Hour * 24)
 	endU := end.Unix()
 
 	params := url.Values{}
-	params.Add("lat", fmt.Sprintf("%f", x))
-	params.Add("lng", fmt.Sprintf("%f", y))
+	params.Add("lat", x)
+	params.Add("lng", y)
 	params.Add("start", fmt.Sprintf("%d", startU))
 	params.Add("end", fmt.Sprintf("%d", endU))
 	params.Add("params", "swellHeight")
 	url := "https://api.stormglass.io/v2/weather/point?"
 
 	meClient := http.Client{
-		Timeout: time.Second * 2, // Timeout after 2 seconds
+		Timeout: time.Second * 10, // Timeout after 2 seconds
 
 	}
 	req, err := http.NewRequest(http.MethodGet, url+params.Encode(), nil)
@@ -155,45 +207,48 @@ func swellAtLocation(x, y float64) Waves {
 	json.Unmarshal([]byte(body), &waves)
 	return waves
 }
-func main() {
 
-	lat := 52.4907
-	long := 4.6023
-	listSwell := swellAtLocation(lat, long)
-	fmt.Println(listSwell)
-	listWind := windAtLocation(lat, long)
-	fmt.Println(listWind)
+func populateConditions(list []Location) {
+	for _, v := range list {
+		listSwell := swellAtLocation(v.Lat, v.Long)
+		listWind := windAtLocation(v.Lat, v.Long)
+		listW := listWind.Hours
+		listS := listSwell.Hours
 
-	listW := listWind.Hours
-	listS := listSwell.Hours
+		psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+			"dbname=%s sslmode=disable",
+			host, port, user, dbname)
+		db, err := sql.Open("postgres", psqlInfo)
+		if err != nil {
+			panic(err)
+		}
+		defer db.Close()
 
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-		"dbname=%s sslmode=disable",
-		host, port, user, dbname)
-	db, err := sql.Open("postgres", psqlInfo)
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
+		err = db.Ping()
+		if err != nil {
+			panic(err)
+		}
 
-	err = db.Ping()
-	if err != nil {
-		panic(err)
-	}
+		fmt.Println("Successfully connected!")
 
-	fmt.Println("Successfully connected!")
-
-	for i, v := range listW {
-		s := listS[i]
-		if s.Time == v.Time {
-			sqlStatement := `
+		for i, u := range listW {
+			s := listS[i]
+			if s.Time == u.Time {
+				sqlStatement := `
     INSERT INTO conditions (spot_id, time_stamp, swell, wind)
     VALUES ($1, $2, $3, $4)`
-			_, err := db.Exec(sqlStatement, 1, v.Time, s.SwellHeight.Icon, v.WindSpeed.Icon)
-			if err != nil {
-				panic(err)
+				_, err := db.Exec(sqlStatement, v.Id, u.Time, s.SwellHeight.Icon, u.WindSpeed.Icon)
+				if err != nil {
+					panic(err)
+				}
 			}
 		}
 	}
+}
+func main() {
+
+	listSpots := getLocation()
+	fmt.Println(listSpots)
+	populateConditions(listSpots)
 
 }
